759c1b7c2614ab5851a1e4ba69d9babb0bb8d750
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-26 13:06:03 -0500

    first commit

---
+++ src/main/java/com/project1/ms_discovery_service/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.project1.ms_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
